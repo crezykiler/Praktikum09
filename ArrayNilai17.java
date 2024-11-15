7fd4a31e68da6480a2c8577b04b33976cf81acb8
zaki setyawan <kiminsetyawan@gmail.com>
2024-11-15 14:37:08 +0700

    ok

--- ArrayNilai17.java
+++ ArrayNilai17.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
-
 public class ArrayNilai17 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int[] nilaiAkhir = new int[10];
-            for (int i= 0; i < 10; i++) {
+            for (int i = 0; i < nilaiAkhir.length; i++) {
                 System.out.print("Masukkan nilai akhir ke- "+i+" : ");
                 nilaiAkhir[i] = sc.nextInt();
             }
 
                 for (int i = 0; i < 10; i++) {
-                    System.out.println("Nilai akhir ke- "+i+" : " +nilaiAkhir[i]);
+                    if (nilaiAkhir[i] > 70) {
+                        System.out.println("Mahasiswa ke-"+i+" lulus!");
+                    }
+
+                    if (nilaiAkhir[i] < 70) {
+                        System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+                    }
                 }
     }
 }
