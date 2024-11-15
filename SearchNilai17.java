d531ee093d5d5b7234e48f5241f6a9d9529bbfed
zaki setyawan <kiminsetyawan@gmail.com>
2024-11-15 16:11:24 +0700

    iya

--- SearchNilai17.java
+++ SearchNilai17.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
-
 public class SearchNilai17 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        int nilai = 0, MHS = 0, key = 0, hasil = 0;
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        nilai = sc.nextInt();
+        int nilaiMHS[] = new int[nilai];
+        for (int i = 0; i < nilaiMHS.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            nilaiMHS[i] = sc.nextInt();
+        }
 
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < nilaiMHS.length; i++) {
+            if (key == nilaiMHS[i]){
+                hasil = (i+1);
                 break;
+            } 
+            else {
+                System.out.println("Nilai yang dicari tidak ditemukan.");
+                return;
             }
         }
+
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-" +hasil);
+        System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-" +hasil);
         System.out.println();
     }
 }
