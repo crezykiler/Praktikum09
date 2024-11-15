322cf393a63bb2c21d15b202f4b61a35ebb0138c
zaki setyawan <kiminsetyawan@gmail.com>
2024-11-15 15:41:40 +0700

    oke

--- ArrayRataNilai17.java
+++ ArrayRataNilai17.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
-
 public class ArrayRataNilai17 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    double total = 0, totalTidakLulus = 0;
+    int MHS = 0, jumlahTidakLulus = 0;
+    double rata2, rata3;
 
-    int[] nilaiMHS = new int[10];
-    double total = 0;
-    double rata2;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        MHS = sc.nextInt();
+    
+    int[] nilaiMHS = new int[MHS];
 
     for (int i = 0; i < nilaiMHS.length; i++) {
         System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -15,9 +18,20 @@ public static void main(String[] args) {
 
         for (int i = 0; i< nilaiMHS.length; i++) {
             total += nilaiMHS[i]; 
-        }
+            if (nilaiMHS[i] < 70) {
+                totalTidakLulus += nilaiMHS[i];
+                jumlahTidakLulus++;
+            }
 
+        }
     rata2 = total/nilaiMHS.length;
     System.out.println("Rata-rata nilai = " + rata2);
+        if (jumlahTidakLulus > 0) {
+            rata3 = totalTidakLulus / jumlahTidakLulus;
+            System.out.println("Rata-rata nilai mahasiswa yang tidak lulus = " + rata3);
+        } else {
+            System.out.println("Tidak ada mahasiswa yang tidak lulus.");
+        }
+    
     }
 }
